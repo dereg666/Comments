7d9651797636e8882b355865577b9207b599d533
b02901157 <b02901157@ntu.edu.tw>
2017-05-06 17:34:37 +0800

    temp

--- src/client/BoardApp.js
+++ src/client/BoardApp.js
@@ -3,10 +3,62 @@ import CommentSection from './CommentSection';
 
 
 class BoardApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      comments: [],
+      addCommentHolder: 'Type to add a comment',
+      addValue: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  // componentDidMount() {
+  //   fetch('/api')
+  //     .then(response => response.json())
+  //     .then((data) => {
+  //       this.setState({ comments: data });
+  //     }).catch((error) => {
+  //       console.log('request failed', error);
+  //     });
+  // }
+  handleCommentChange(event, section) {
+    const tempValue = this.state.addValue;
+    tempValue[section] = event.target.value;
+    this.setState({ addValue: tempValue });
+  }
+  textFocus() {
+    this.setState({ addCommentHolder: '' });
+  }
+  textBlur() {
+    this.setState({ addCommentHolder: 'Type to add a comment' });
+  }
+  submitFunction() {
+    if (this.state.addCommentValue) {
+      const addList = {
+        todoName: this.state.lists.length.toString() + ' ' + this.state.addListValue,
+        todoItems: [],
+      };
+      const temp = this.state.lists;
+      temp.push(addList);
+      this.setState({ showMode: (temp.length + 2) });
+      this.setState({ lists: temp });
+      this.setState({ addListValue: '' });
+    }
+  }
   render() {
     return (
       <div>
-        hello
+        <div className="header">
+          <h1> Comment Board </h1>
+        </div>
+        <div className="App">
+          <div className="InputBox">
+            <input />
+          </div>
+          <div className="Comments">
+            {this.state.comments.map(c => <CommentSection />)}
+          </div>
+        </div>
       </div>
     );
   }
