86b8ae168f69224e618d2a8a54df45ad1e9b1b01
b02901157 <b02901157@ntu.edu.tw>
2017-05-06 14:33:19 +0800

    first commit

---
+++ src/client/BoardApp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import CommentSection from './CommentSection';
+
+
+class BoardApp extends Component {
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
+
+export default BoardApp;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BoardApp from './client/BoardApp';
+import './client/index.css';
+
+ReactDOM.render(
+  <div>
+    <BoardApp />
+  </div>,
+  document.getElementById('root'),
+);
